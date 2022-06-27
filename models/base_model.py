094a32b8ba230f1526b12777cca5a44457c549ba
Eiden <eidnegabriel07@gmail.com>
2022-06-27 15:12:51 -0400

    str method

--- models/base_model.py
+++ models/base_model.py
@@ -10,6 +10,9 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
     def save(self):
         self.updated_at = datetime.now()
 
