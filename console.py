a9163b0c4ba41117c3ef8068205df351ccaccf9a
Eiden <eidnegabriel07@gmail.com>
2022-06-28 17:56:15 -0400

    task 7 even if it doesnt work

--- console.py
+++ console.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import cmd
+import json
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -12,6 +14,42 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, args):
         'Quit command to exit the program\n'
         exit()
+
+    def do_create(self, args):
+        'Creates a new instance of BaseModel, saves it (to the JSON file)\n'
+        if not args:
+            print("** class name missing **")
+        if args != BaseModel.__name__:
+            print("** class doesn't exist **")
+        else:
+            new = BaseModel()
+            #json.dump(new, file.json)
+            print(new.id)
+
+    def do_show(self, *args):
+        'Prints the string representation of an instance based on the class name and id\n'
+        new = BaseModel()
+        #arg = args.split()
+        if not args[0]:
+            print("** class name missing **")
+        elif args[0] != BaseModel.__name__:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif len(args) == 1 or args[1] != new.id:
+            print("** no instance found **")
+        else:
+            print(args)
+
+   # ------------- PreCmd and PostCmd -------
+
+    """
+    def precmd(self, line):
+        line = line.lower()
+        if self.file:
+            print(line, file=self.file)
+        return line
+    """
 
     def postcmd(self, stop, line):
         if cmd.Cmd.do_help:
