1e793194c5251b988958d3e9ffeebaa1c7cb7502
ChrissLind <chrislindino@gmail.com>
2022-06-29 18:06:34 -0400

    task 9 classes created

---
+++ models/place.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Defines a place class."""
+import models
+from models.base_model import BaseModel
+
+class Place(BaseModel):
+    """Represents a place.
+
+        inherits from BaseModel
+        Attributes:
+            city_id:
+            user_id:
+            name: Name of the city.
+            description:
+            number_rooms:
+            number_bathrooms:
+            max_guest:
+            price_by_night:
+            latitude:
+            amenity_ids:
+    """
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0.0
+    latitude = 0.0
+    amenity_ids = []
---
+++ models/review.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Defines a review class."""
+import models
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """Represents a review.
+
+        inherits from BaseModel
+        Attributes:
+            place_id: string containing the Place ID.
+            user_id: string containing the User ID.
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
