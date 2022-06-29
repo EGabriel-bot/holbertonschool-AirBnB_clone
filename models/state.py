9d9f1e5f86a1be0235002dc4e73af338066f9bb8
ChrissLind <chrislindino@gmail.com>
2022-06-29 17:02:00 -0400

    states

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Defines a state class."""
+import models
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """Represents a state.
+
+        inherits from BaseModel
+        Attributes:
+            name: Name of the state.
+    """
+    name = ""
--- models/user.py
+++ models/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Defines the User class"""
+"""Defines the User class."""
 from models.base_model import BaseModel
 
 class User(BaseModel):
