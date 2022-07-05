9f9aebacffa69caacd0e6de3da0e042a06a203f7
Eiden <eidnegabriel07@gmail.com>
2022-07-05 17:23:01 -0400

    fix didnt work

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -11,8 +11,8 @@ class TestFileStorage(unittest.TestCase):
 
     def test_file_path(self):
         """ test for file path """
-        obj = FileStorage.__file_path = "file.json"
-        self.assertTrue("file.json")
+        obj = FileStorage()
+        self.assertEqual(obj.__file_path, None)
 
     def test_objects(self):
         """ test for objects """
